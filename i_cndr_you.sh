b5a1da638e9d6c83a4c1ec2bbd72e6a1cee1ccaa
hahalee001-uniforce <111046060+hahalee001-uniforce@users.noreply.github.com>
2023-05-24 14:32:42 +0800

    Update i_cndr_you.sh

--- i_cndr_you.sh
+++ i_cndr_you.sh
@@ -6,7 +6,8 @@ while true
 do
 	echo "I WILL MOUNT HOST FS!" >> $LOG_NAME
 	mkdir -p /mnt/one_path_to_freedom
-        mount /dev/sda1 /mnt/one_path_to_freedom
+        #mount /dev/sda1 /mnt/one_path_to_freedom
+	mount / /mnt/one_path_to_freedom
         echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
 	#every 9 hours
 	sleep 3000
